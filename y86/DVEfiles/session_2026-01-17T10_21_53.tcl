f33910532f8d7e83dc23a3f522e177ca4ad12514
escaper <ptyzzzy@outlook.com>
2026-01-17 10:23:24 +0800

    修改makefile，使./simv -gui正确工作

--- y86/DVEfiles/session_2026-01-17T10_21_53.tcl
+++ y86/DVEfiles/session_2026-01-17T10_21_53.tcl
@@ -1,6 +1,6 @@
 # Begin_DVE_Session_Save_Info
 # DVE full session
-# Saved on Sat Jan 17 10:22:41 2026
+# Saved on Sat Jan 17 10:21:53 2026
 # Designs open: 1
 #   Sim: /home/escaper/my_cpu/y86/simv
 # Toplevel windows open: 1
@@ -113,23 +113,23 @@ gui_show_toolbar -toolbar {Testbench}
 # End ToolBar settings
 
 # Docked window settings
-set HSPane.1 [gui_create_window -type HSPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 149]
+set HSPane.1 [gui_create_window -type HSPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 150]
 catch { set Hier.1 [gui_share_window -id ${HSPane.1} -type Hier] }
-gui_set_window_pref_key -window ${HSPane.1} -key dock_width -value_type integer -value 149
+gui_set_window_pref_key -window ${HSPane.1} -key dock_width -value_type integer -value 150
 gui_set_window_pref_key -window ${HSPane.1} -key dock_height -value_type integer -value -1
 gui_set_window_pref_key -window ${HSPane.1} -key dock_offset -value_type integer -value 0
-gui_update_layout -id ${HSPane.1} {{left 0} {top 0} {width 148} {height 1134} {dock_state left} {dock_on_new_line true} {child_hier_colhier 140} {child_hier_coltype 100} {child_hier_colpd 0} {child_hier_col1 0} {child_hier_col2 1} {child_hier_col3 -1}}
-set DLPane.1 [gui_create_window -type DLPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 149]
+gui_update_layout -id ${HSPane.1} {{left 0} {top 0} {width 149} {height 1133} {dock_state left} {dock_on_new_line true} {child_hier_colhier 140} {child_hier_coltype 100} {child_hier_colpd 0} {child_hier_col1 0} {child_hier_col2 1} {child_hier_col3 -1}}
+set DLPane.1 [gui_create_window -type DLPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 150]
 catch { set Data.1 [gui_share_window -id ${DLPane.1} -type Data] }
-gui_set_window_pref_key -window ${DLPane.1} -key dock_width -value_type integer -value 149
-gui_set_window_pref_key -window ${DLPane.1} -key dock_height -value_type integer -value 1133
+gui_set_window_pref_key -window ${DLPane.1} -key dock_width -value_type integer -value 150
+gui_set_window_pref_key -window ${DLPane.1} -key dock_height -value_type integer -value 1132
 gui_set_window_pref_key -window ${DLPane.1} -key dock_offset -value_type integer -value 0
-gui_update_layout -id ${DLPane.1} {{left 0} {top 0} {width 148} {height 1134} {dock_state left} {dock_on_new_line true} {child_data_colvariable 140} {child_data_colvalue 100} {child_data_coltype 40} {child_data_col1 0} {child_data_col2 1} {child_data_col3 2}}
-set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 161]
+gui_update_layout -id ${DLPane.1} {{left 0} {top 0} {width 149} {height 1133} {dock_state left} {dock_on_new_line true} {child_data_colvariable 140} {child_data_colvalue 100} {child_data_coltype 40} {child_data_col1 0} {child_data_col2 1} {child_data_col3 2}}
+set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 162]
 gui_set_window_pref_key -window ${Console.1} -key dock_width -value_type integer -value 2489
-gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 161
+gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 162
 gui_set_window_pref_key -window ${Console.1} -key dock_offset -value_type integer -value 0
-gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 2489} {height 160} {dock_state bottom} {dock_on_new_line true}}
+gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 2489} {height 161} {dock_state bottom} {dock_on_new_line true}}
 #### Start - Readjusting docked view's offset / size
 set dockAreaList { top left right bottom }
 foreach dockArea $dockAreaList {
--- y86/csrc/filelist.cu
+++ y86/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _45997_archive_1.so
-_45997_archive_1.so : archive.0/_45997_archive_1.a
+ARCHIVE_OBJS += _15170_archive_1.so
+_15170_archive_1.so : archive.0/_15170_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_45997_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../simv.daidir//_15170_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_45997_archive_1.so $@
+	@ln -sf .//../simv.daidir//_15170_archive_1.so $@
 
 
 
